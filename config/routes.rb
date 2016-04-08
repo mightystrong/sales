7a63c07dd557f33f53069eeaa88e87f17bbe567d
Michael Price <mike@mightystrongmedia.com>
2016-04-08 10:17:15 -0600

    Changed the app route to the Product Index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :sales
   resources :products
   devise_for :users
+
+  root 'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
